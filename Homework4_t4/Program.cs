8042a3cae1d3d6ab01e7b913e652d340769c0f32
Anna <anka22@msn.com>
2023-02-10 22:27:37 +0300

    Задача 54

---
+++ Homework4_t4/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2 
+
+int[,] array = new int[3, 4];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Числа по убыванию: ");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int z = 0; z < array.GetLength(1) - 1; z++)
+        {
+            if (array[i, z] < array[i, z + 1])
+            {
+                int number = array[i, z + 1];
+                array[i, z + 1] = array[i, z];
+                array[i, z] = number;
+            }
+
+        }
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
