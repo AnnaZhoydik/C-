fef128231bdffa4c1ab7f612bb5c168bcfe0df26
Anna <anka22@msn.com>
2023-02-10 22:28:15 +0300

    Задача 56

---
+++ Homework4_t5/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+int[,] array = new int[4, 4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("С наименьшей суммой: ");
+int minLine = 0;
+int minSumLine = 0;
+int sumLine = 0;
+for (int i = 0; i < array.GetLength(1); i++)
+{
+    minLine = minLine + array[0, i];
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+
+        sumLine = sumLine + array[i, j];
+    if (sumLine < minLine)
+    {
+        minLine = sumLine;
+        minSumLine = i;
+    }
+    sumLine = 0;
+
+}
+Console.Write($"{minSumLine + 1} строка");
+
+
